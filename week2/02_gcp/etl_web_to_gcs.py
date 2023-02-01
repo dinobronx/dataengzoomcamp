528923e40e18df2be95a973a992ea57530ae8e16
deebeecode <deebeecode@gmail.com>
2023-01-31 19:36:55 -0500

    Add sample workflow for gcs to bigquery

--- week2/02_gcp/etl_web_to_gcs.py
+++ week2/02_gcp/etl_web_to_gcs.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
