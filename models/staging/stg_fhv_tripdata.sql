cef9ed22cf42e573173ebbb57244d849d7eb8947
dbronola@gmail.com <dbronola@gmail.com>
2023-02-19 08:02:00 +0000

    uncomment limit

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -14,8 +14,8 @@ select
     cast(SR_Flag as integer) as sr_flag,
     Affiliated_base_number as affiliated_base_num
 from tripdata
--- {% if var('is_test_run', default=true) %}
+{% if var('is_test_run', default=true) %}
 
--- limit 100
+limit 100
 
--- {% endif %}
+{% endif %}
